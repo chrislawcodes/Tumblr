9f83fc0e7114decce07591bff84138d01ccbdc6e
chrislawcodes <chrislaw@gmail.com>
2015-10-07 21:59:53 -0700

    Milestone 8 Complete

--- TabBarViewController.swift
+++ TabBarViewController.swift
@@ -12,11 +12,25 @@ class TabBarViewController: UIViewController {
 
     @IBOutlet weak var contentView: UIView!
     
+    var HomeViewController: UIViewController!
+    var SearchViewController: UIViewController!
+    var ComposeViewController: UIViewController!
+    var AccountViewController: UIViewController!
+    var TrendingViewController: UIViewController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let storyboard = UIStoryboard(name:"Main", bundle: nil)
+        
+        HomeViewController = storyboard.instantiateViewControllerWithIdentifier ("HomeViewController")
+        SearchViewController = storyboard.instantiateViewControllerWithIdentifier ("SearchViewController")
+        ComposeViewController = storyboard.instantiateViewControllerWithIdentifier ("ComposeViewController")
+        AccountViewController = storyboard.instantiateViewControllerWithIdentifier ("AccountViewController")
+        TrendingViewController =  storyboard.instantiateViewControllerWithIdentifier ("TrendingViewController")
+        
 
-        // Do any additional setup after loading the view.
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +38,33 @@ class TabBarViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    @IBAction func onHomeButton(sender: AnyObject) {
+        contentView.addSubview(HomeViewController.view)
+        
+    }
+
+    @IBAction func onSearchButton(sender: AnyObject) {
+        contentView.addSubview(SearchViewController.view)
+    }
+    
+    
+    @IBAction func onComposeButton(sender: AnyObject) {
+        contentView.addSubview(ComposeViewController.view)
+    }
+    
+    @IBAction func onAccountButton(sender: AnyObject) {
+        contentView.addSubview(AccountViewController.view)
+    }
+
+    @IBAction func onTrendingButton(sender: AnyObject) {
+        contentView.addSubview(TrendingViewController.view)
+    }
+    
+    
+    
+    
+
 
     /*
     // MARK: - Navigation
